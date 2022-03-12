909d67ddb83f6e51e707a7ddb9bd35750913357e
Simon Vriesema <simon.vriesema@hva.nl>
2022-03-12 18:24:46 +0100

    bezig met ondernemer registreren backend

---
+++ src/assets/js/repositories/entrepreneursRepository.js
@@ -0,0 +1,32 @@
+/**
+ * Repository voor Entity Entrepreneurs
+ */
+import {NetworkManager} from "../framework/utils/networkManager.js";
+
+export class EntrepreneursRepository {
+    #networkManager;
+    #route;
+
+    constructor() {
+        this.#route = "/ondernemer";
+        this.#networkManager = new NetworkManager();
+    }
+
+    // createOrder(bestelnummer,verzendnaam, verzendadres, verzendplaats, verzend_postcode, geschatte_bezorgdatum, verzend_datum,
+    //             bezorgkosten, opmerking, Bezorger_bezorger_id, Klant_klantnummer, Ondernemer_ondernemer_id, besteldatum) {
+    //     this.#networkManager.doRequest(this.#route, "POST",
+    //         {bestelnummer: bestelnummer,
+    //             verzendnaam: verzendnaam,
+    //             verzendadres: verzendadres,
+    //             verzendplaats: verzendplaats,
+    //             verzend_postcode: verzend_postcode,
+    //             geschatte_bezorgdatum: geschatte_bezorgdatum,
+    //             verzend_datum: verzend_datum,
+    //             bezorgkosten: bezorgkosten,
+    //             opmerking: opmerking,
+    //             Bezorger_bezorger_id: Bezorger_bezorger_id,
+    //             Klant_klantnummer: Klant_klantnummer,
+    //             Ondernemer_ondernemer_id: Ondernemer_ondernemer_id,
+    //             besteldatum: besteldatum})
+    // }
+}
