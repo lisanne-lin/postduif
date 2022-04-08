8eeb9818afb2f4ba65cf4a6d4aac52c19849d80f
Simon Vriesema <simon.vriesema@hva.nl>
2022-04-08 15:46:59 +0200

    Bezig met dashboatrd :)

--- src/assets/js/controllers/dashboardController.js
+++ src/assets/js/controllers/dashboardController.js
@@ -1,20 +1,23 @@
-import { Controller } from "./controller.js";
+import {App} from "../app.js";
+import {Controller} from "./controller.js";
+import {OrdersRepository} from "../repositories/ordersRepository.js";
 
 export class DashboardController extends Controller {
 
+    #ordersRepository
     #dashboardView;
 
     constructor()  {
         super();
-
+        this.#ordersRepository = new OrdersRepository();
         this.#setup();
     }
 
     async #setup() {
         this.#dashboardView = await super.loadHtmlIntoContent("html_views/dashboard.html");
-
-        const testPar = this.#dashboardView.querySelector(".test-class-p");
-
-        console.log(testPar);
+        document.querySelector(".navbar").style.display = "block";
+        document.querySelector("#nav-orders").className = "nav-link";
+        document.querySelector("#nav-dash").className = "nav-link bg-success text-light";
+        document.querySelector("#nav-settings").className = "nav-link";
     }
 }
