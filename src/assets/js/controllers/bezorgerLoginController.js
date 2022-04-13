1b231e10bbe2c5f82e11772a83ba28911c3125f4
parkes <joy.park@hva.nl>
2022-04-13 14:07:17 +0200

    inlog bezorger --> bezorger bestellig lijst

--- src/assets/js/controllers/bezorgerLoginController.js
+++ src/assets/js/controllers/bezorgerLoginController.js
@@ -43,7 +43,8 @@ export class BezorgerLoginController extends Controller{
 
             //let the session manager know we are logged in by setting the username, never set the password in localstorage
             App.sessionManager.set("username", user.emailadres);
-            App.loadController(App.CONTROLLER_WELCOME);
+            App.loadController(App.CONTROLLER_BEZORGER_BESTELLING);
+
             document.querySelector(".sidebar-container").style.display = "block";
         } catch(error) {
             //if unauthorized error code, show error message to the user
