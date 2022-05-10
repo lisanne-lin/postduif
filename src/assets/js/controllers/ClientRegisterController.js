ee16a05083767d5e3b5500f96b5686327870323c
Simon Vriesema <simon.vriesema@hva.nl>
2022-05-10 23:21:51 +0200

    bezig met client register

--- src/assets/js/controllers/ClientRegisterController.js
+++ src/assets/js/controllers/ClientRegisterController.js
@@ -1,19 +1,24 @@
 import {Controller} from "./controller.js";
-import {App} from "../app.js";
 import {CustomersRepository} from "../repositories/customersRepository.js";
+import {OrdersRepository} from "../repositories/ordersRepository.js";
+import {App} from "../app.js";
 
 export class ClientRegisterController extends Controller {
     #clientRegisterView;
     #customersRepository;
+    #ordersRepository;
 
     constructor() {
         super();
         this.#customersRepository = new CustomersRepository();
+        this.#ordersRepository = new OrdersRepository();
         this.#setupView();
     }
 
     async #setupView() {
-        this.#clientRegisterView = await super.loadHtmlIntoContent("html_views/client_register.html")
+        App.loadController(App.CONTROLLER_NAVBAR_CLIENT);
+
+        this.#clientRegisterView = await super.loadHtmlIntoContent("html_views/register_client.html")
 
         document.querySelector(".navbar").style.display = "block";
     }
