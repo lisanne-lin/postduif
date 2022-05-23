bff8bedbcb7b9d35cc715bb559194acfffbe8633
parkes <joy.park@hva.nl>
2022-05-23 22:22:11 +0200

    (tijdelijke) button toegevoegd voor reviews, database voor reviews, en een beetje js

--- src/assets/js/controllers/reviewClientController.js
+++ src/assets/js/controllers/reviewClientController.js
@@ -1,7 +1,7 @@
-import { App } from "../app.js";
-import { Controller } from "./controller.js";
+import {App} from "../app.js";
+import {Controller} from "./controller.js";
 
-export class ReviewClientController extends Controller{
+export class ReviewClientController extends Controller {
     #reviewClient
 
     constructor() {
@@ -20,8 +20,13 @@ export class ReviewClientController extends Controller{
 
         const anchors = this.#reviewClient.querySelectorAll("a.nav-link");
 
+        this.#reviewClient.querySelector("#buttonSend").addEventListener("click",
+            (event) => this.#commend(event));
+
         // //set click listener on each anchor
         anchors.forEach(anchor => anchor.addEventListener("click", (event) => this.#handleClickNavigationItem(event)))
+
+
     }
 
 
@@ -30,7 +35,7 @@ export class ReviewClientController extends Controller{
         const clickedAnchor = event.target;
         const controller = clickedAnchor.dataset.controller;
 
-        if(typeof controller === "undefined") {
+        if (typeof controller === "undefined") {
             console.error("No data-controller attribute defined in anchor HTML tag, don't know which controller to load!")
             return false;
         }
@@ -39,4 +44,11 @@ export class ReviewClientController extends Controller{
         return false;
     }
 
+
+    #commend(event) {
+
+        let theCommand = document.getElementById("reviewBox");
+        alert(theCommand.value);
+        return undefined;
+    }
 }
