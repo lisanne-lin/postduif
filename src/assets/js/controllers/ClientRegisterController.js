99b11bc8abba07e235c01ae5ff18cff6a566e161
Simon Vriesema <simon.vriesema@hva.nl>
2022-06-09 16:46:49 +0200

    ClientRegisterController.js is in english now

--- src/assets/js/controllers/ClientRegisterController.js
+++ src/assets/js/controllers/ClientRegisterController.js
@@ -31,70 +31,74 @@ export class ClientRegisterController extends Controller {
         document.querySelector("#nav-logout").style.display = "none";
     }
 
+    /**
+     * Function to save account to DB
+     * @param event = event
+     * @returns {Promise<void>}
+     */
     async #saveAccount(event) {
         event.preventDefault();
 
-        const voornaam = this.#clientRegisterView.querySelector(
+        const FIRST_NAME = this.#clientRegisterView.querySelector(
             "#exampleInputNaamEigenaar"
         ).value;
-        const achternaam = this.#clientRegisterView.querySelector(
+        const LAST_NAME = this.#clientRegisterView.querySelector(
             "#exampleInputAchternaam"
         ).value;
-        const email =
-            this.#clientRegisterView.querySelector("#exampleInputEmail").value;
-        const wachtwoord = this.#clientRegisterView.querySelector(
+        const EMAIL = this.#clientRegisterView.querySelector("#exampleInputEmail").value;
+        const PASSWORD = this.#clientRegisterView.querySelector(
             "#exampleInputPassword"
         ).value;
-        const telefoonnummer = this.#clientRegisterView.querySelector(
+        const PHONENUMBER = this.#clientRegisterView.querySelector(
             "#exampleInputPhonenumber"
         ).value;
-        const adres = this.#clientRegisterView.querySelector(
+        const ADDRESS = this.#clientRegisterView.querySelector(
             "#exampleInputAdress"
         ).value;
-        const plaats = this.#clientRegisterView.querySelector(
+        const PLACE = this.#clientRegisterView.querySelector(
             "#exampleInputResidence"
         ).value;
-        const postcode = this.#clientRegisterView.querySelector(
+        const ZIP = this.#clientRegisterView.querySelector(
             "#exampleInputPostcode"
         ).value;
 
-        const errorBox = this.#clientRegisterView.querySelector(".error");
+        const ERROR_BOX = this.#clientRegisterView.querySelector(".error");
 
-        const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/;
-        const regexZip = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i;
-        const adresRegex = /^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i;
+        const REGEX_EMAIL = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const REGEX_PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/;
+        const REGEX_ZIP = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i;
+        const REGEX_ADDRESS = /^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i;
 
-        if (voornaam === null || voornaam === "") {
-            errorBox.innerHTML = "Name can't be empty";
-        } else if (voornaam === null || voornaam === "") {
-            errorBox.innerHTML = "Name can't be empty";
-        } else if (achternaam === null || achternaam === "") {
-            errorBox.innerHTML = "Surname can't be empty";
-        } else if (!email.match(regexEmail)) {
-            errorBox.innerHTML = "Please enter a valid email address, example:  user@gmail.com";
-        } else if (!wachtwoord.match(regexPassword)) {
-            errorBox.innerHTML = "Please enter a password with minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
-        } else if (!adres.match(adresRegex)) {
-            errorBox.innerHTML = "Please enter a correct address";
-        } else if (telefoonnummer === null || telefoonnummer === "") {
-            errorBox.innerHTML = "Phone number can't be empty";
-        } else if (plaats === null || plaats === "") {
-            errorBox.innerHTML = "Place can't be empty";
-        }  else if (!postcode.match(regexZip)) {
-            errorBox.innerHTML = "Please enter a zip code";
+        if (FIRST_NAME === null || FIRST_NAME === "") {
+            ERROR_BOX.innerHTML = "Name can't be empty";
+        } else if (FIRST_NAME === null || FIRST_NAME === "") {
+            ERROR_BOX.innerHTML = "Name can't be empty";
+        } else if (LAST_NAME === null || LAST_NAME === "") {
+            ERROR_BOX.innerHTML = "Surname can't be empty";
+        } else if (!EMAIL.match(REGEX_EMAIL)) {
+            ERROR_BOX.innerHTML = "Please enter a valid email address, example:  user@gmail.com";
+        } else if (!PASSWORD.match(REGEX_PASSWORD)) {
+            ERROR_BOX.innerHTML = "Please enter a password with minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
+        } else if (!ADDRESS.match(REGEX_ADDRESS)) {
+            ERROR_BOX.innerHTML = "Please enter a correct address";
+        } else if (PHONENUMBER === null || PHONENUMBER === "") {
+            ERROR_BOX.innerHTML = "Phone number can't be empty";
+        } else if (PLACE === null || PLACE === "") {
+            ERROR_BOX.innerHTML = "Place can't be empty";
+        } else if (!ZIP.match(REGEX_ZIP)) {
+            ERROR_BOX.innerHTML = "Please enter a zip code";
         } else {
-            errorBox.innerHTML = "";
+            ERROR_BOX.innerHTML = "";
             await this.#customersRepository.createCustomer(
                 null,
-                voornaam,
-                achternaam,
-                email,
-                telefoonnummer,
-                plaats,
-                adres,
-                postcode,
-                wachtwoord
+                FIRST_NAME,
+                LAST_NAME,
+                EMAIL,
+                PHONENUMBER,
+                PLACE,
+                ADDRESS,
+                ZIP,
+                PASSWORD
             );
         }
     }
