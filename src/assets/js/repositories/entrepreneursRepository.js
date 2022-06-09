f106e6f347bfcb5aafd6447d362f7585e2573cee
Simon Vriesema <simon.vriesema@hva.nl>
2022-06-09 20:29:17 +0200

    translated my repositories

--- src/assets/js/repositories/entrepreneursRepository.js
+++ src/assets/js/repositories/entrepreneursRepository.js
@@ -8,31 +8,31 @@ export class EntrepreneursRepository {
     #route;
 
     constructor() {
-        this.#route = "/ondernemer";
+        this.#route = "/entrepreneur";
         this.#networkManager = new NetworkManager();
     }
 
-    async getUserIdByEmail(emailadres) {
-        return await this.#networkManager.doRequest(`${this.#route}/getIdFromEmailAdres/${emailadres}`, "GET");
+    async getUserIdByEmail(emailaddress) {
+        return await this.#networkManager.doRequest(`${this.#route}/getIdFromemailaddress/${emailaddress}`, "GET");
     }
 
-    async createEntrepreneur(ondernemer_id, naam, eigenaar, adres, plaats, postcode, telefoonnummer, emailadres, wachtwoord) {
+    async createEntrepreneur(entrepreneur_id, name, owner, address, place, zip, phonenumber, emailaddress, password) {
         this.#networkManager.doRequest(this.#route, "POST",
             {
-                ondernemer_id: ondernemer_id,
-                naam: naam,
-                eigenaar: eigenaar,
-                adres: adres,
-                plaats: plaats,
-                postcode: postcode,
-                telefoonnummer: telefoonnummer,
-                emailadres: emailadres,
-                wachtwoord: wachtwoord
+                entrepreneur_id: entrepreneur_id,
+                name: name,
+                owner: owner,
+                address: address,
+                place: place,
+                zip: zip,
+                phonenumber: phonenumber,
+                emailaddress: emailaddress,
+                password: password
             })
     }
 
-    async getEntrepreneurById(ondernemer_id) {
-        return await this.#networkManager.doRequest(`${this.#route}/getentrepreneur/${ondernemer_id}`, "GET");
+    async getEntrepreneurById(entrepreneur_id) {
+        return await this.#networkManager.doRequest(`${this.#route}/getentrepreneur/${entrepreneur_id}`, "GET");
     }
 
 
